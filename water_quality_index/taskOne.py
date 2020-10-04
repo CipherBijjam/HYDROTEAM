c600973d59c8f720b1d164226bd11dd4af48abd4
cipherBijjam <cipher.bsd@gmail.com>
2020-10-04 15:08:45 +0530

    Added CSV functionality to first task

--- water_quality_index/taskOne.py
+++ water_quality_index/taskOne.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.filedialog as tkfd
+import pandas as pd
 
 from selectPage import SelectPage
 from vars import Vars
@@ -31,6 +33,8 @@ class TaskOne(SelectPage):
         self.cal_value = tk.StringVar()
         self.cal_value.set("Not Calculated")
 
+
+
         SelectPage.__init__(self, parent, controller)
         # self.initUI(parent, controller)
 
@@ -51,6 +55,59 @@ class TaskOne(SelectPage):
 
         self.curr_value = tk.Label(self, textvariable = self.cal_value, font = Vars.LABEL_FONT)
         self.curr_value.place(x = 500, y = 350)
+
+        # CSV File
+        self.csv_text = tk.StringVar()
+        self.csv_text.set("CSV File Upload")
+        self.csv_label = tk.Label(self, textvariable = self.csv_text, font = Vars.LABEL_FONT)
+        self.csv_label.place(x = 200, y = 450)
+
+        self.fileInput = tk.Entry(self)
+        self.fileInput.place(x = 200, y = 500, w = 200)
+
+        self.chooseFile = tk.Button(self, text = "Input File", 
+        command =  lambda: self.open_file(), 
+        padx = 10,
+        pady = 10)
+        self.chooseFile.place(x = 450, y = 500, width = 125, height = 35)
+
+        self.fileOutput = tk.Entry(self)
+        self.fileOutput.place(x = 200, y = 550, w = 200)
+
+        self.calculate2 = tk.Button(self, text = "Calculate", 
+        command =  lambda: self._calculate_csv_output(), 
+        padx = 10,
+        pady = 10)
+        self.calculate2.place(x = 450, y = 550, width = 125, height = 35)
+
+
+    def _calculate_csv_output(self):
+        inputFilename = self.fileInput.get()
+        outputFilename = self.fileOutput.get()
+        if inputFilename == "" or outputFilename == "":
+            self.csv_text.set("ERROR!! Plz Enter Both Fields !!RORRE")
+        else:
+            df = pd.read_csv(inputFilename)
+            values = []
+            for i in range(0, len(df)):
+                sum = 0
+                for word in self.words:
+                    sum += (df[word][i] * self.weights[word])
+                values.append(sum)       
+            df['WQI'] = values
+            df.to_csv(outputFilename, index = False)
+            self.csv_text.set("DONE")
+            self.fileInput.delete(0, tk.END)
+            self.fileOutput.delete(0, tk.END)
+
+     
+
+    def open_file(self):
+        filename = tkfd.askopenfilename(filetypes =[('CSV Files', '*.csv')])
+        if filename != () and filename != "":
+            self.fileInput.delete(0, tk.END)
+            self.fileInput.insert(0, filename)
+        
 
     def validate_float(self, inp, empty = 0):
         try:
